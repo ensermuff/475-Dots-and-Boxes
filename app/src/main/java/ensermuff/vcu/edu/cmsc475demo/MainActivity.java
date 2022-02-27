02108f0e17ce42d8acaffaaa03ecac7aa27c01c5
Firaol Ensermu <Firaolfayeensermu@gmail.com>
2022-02-26 22:56:16 -0500

    first commit

--- app/src/main/java/ensermuff/vcu/edu/cmsc475demo/MainActivity.java
+++ app/src/main/java/ensermuff/vcu/edu/cmsc475demo/MainActivity.java
@@ -3,6 +3,7 @@ package ensermuff.vcu.edu.cmsc475demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView firstImage = (ImageView) findViewById(R.id.SquaresLogo);
+        int imageResource = getResources().getIdentifier("@drawable/squares", null,this.getPackageName());
+        firstImage.setImageResource(imageResource);
+
+        ImageView secondImage = (ImageView) findViewById(R.id.Vcu);
+        int imageResource2 = getResources().getIdentifier("@drawable/vcu", null,this.getPackageName());
+        secondImage.setImageResource(imageResource2);
+
+
     }
 }
