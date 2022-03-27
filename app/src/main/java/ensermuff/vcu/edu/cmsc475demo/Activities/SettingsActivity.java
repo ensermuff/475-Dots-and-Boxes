6edbb07b4e36e07ca9a2f96f64734fff14a54215
Alexander Harris <awhofficial@gmail.com>
2022-03-27 11:44:45 -0400

    - Fixed a little bit of the setting page

--- app/src/main/java/ensermuff/vcu/edu/cmsc475demo/Activities/SettingsActivity.java
+++ app/src/main/java/ensermuff/vcu/edu/cmsc475demo/Activities/SettingsActivity.java
@@ -19,15 +19,15 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
-//        ImageButton backBtn = findViewById(R.id.settingsBackBtn);
-//        backBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                // Intent is an action being requested that the device should try to perform
-//                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
-//                //startActivity() is used to launch another activity
-//                startActivity(startIntent);
-//            }
-//        });
+        ImageButton backBtn = findViewById(R.id.settingsBackBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent is an action being requested that the device should try to perform
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                //startActivity() is used to launch another activity
+                startActivity(startIntent);
+            }
+        });
     }
 }
