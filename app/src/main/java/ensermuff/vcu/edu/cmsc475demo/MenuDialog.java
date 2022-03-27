ddde410712d595e5c0a48b1f385cfd1572cc4131
Firaol Ensermu <Firaolfayeensermu@gmail.com>
2022-03-27 11:59:03 -0400

    Added music

---
+++ app/src/main/java/ensermuff/vcu/edu/cmsc475demo/MenuDialog.java
@@ -0,0 +1,26 @@
+package ensermuff.vcu.edu.cmsc475demo;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class MenuDialog extends AppCompatDialogFragment {
+
+//    public void openDialog(){
+//        final Dialog dialog = new Dialog(getContext());
+//        dialog.setContentView(R.layout.menudialog);
+//        dialog.setTitle(R.string.dialog_title);
+//        dialog.show();
+//    }
+
+//  public Dialog onCreateDialog(Bundle savedInstanceState){
+//      AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MenuDialog);//
+//      builder.setTitle(R.string.dialog_title);
+//      builder.setMessage("TESTING!");
+//      return builder.create();
+//  }
+}
+
