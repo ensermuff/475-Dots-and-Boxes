9709ad63e936a7081690ccb7ee41a0e06a4fc37e
Firaol Ensermu <Firaolfayeensermu@gmail.com>
2023-04-30 22:44:33 -0400

    Recorded a new Espresso Test for SettingsActivity

--- app/src/androidTest/java/ensermuff/vcu/edu/cmsc475demo/Activities/SettingsActivityTest.java
+++ app/src/androidTest/java/ensermuff/vcu/edu/cmsc475demo/Activities/SettingsActivityTest.java
@@ -5,10 +5,11 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
@@ -25,6 +26,7 @@ import androidx.test.filters.LargeTest;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -69,7 +71,7 @@ public class SettingsActivityTest {
                                         1),
                                 1),
                         isDisplayed()));
-        appCompatEditText2.perform(replaceText("Stong"), closeSoftKeyboard());
+        appCompatEditText2.perform(replaceText("Mark"), closeSoftKeyboard());
 
         ViewInteraction materialButton = onView(
                 allOf(withId(R.id.confirm), withText("Confirm"),
@@ -80,6 +82,30 @@ public class SettingsActivityTest {
                                 2),
                         isDisplayed()));
         materialButton.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.player1Name), withText("Jack"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        editText.check(matches(isDisplayed()));
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.player2Name), withText("Mark"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        editText2.check(matches(withText("Mark")));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.confirm), withText("CONFIRM"),
+                        withParent(withParent(withId(R.id.constraintLayout10))),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.confirm), withText("CONFIRM"),
+                        withParent(withParent(withId(R.id.constraintLayout10))),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
 
         ViewInteraction appCompatImageButton2 = onView(
                 allOf(withId(R.id.p1BlueBtn),
@@ -92,12 +118,12 @@ public class SettingsActivityTest {
         appCompatImageButton2.perform(click());
 
         ViewInteraction appCompatImageButton3 = onView(
-                allOf(withId(R.id.p2PurpleBtn),
+                allOf(withId(R.id.p2GreenBtn),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         1),
-                                8),
+                                4),
                         isDisplayed()));
         appCompatImageButton3.perform(click());
 
@@ -112,6 +138,46 @@ public class SettingsActivityTest {
         appCompatImageButton4.perform(click());
 
         ViewInteraction appCompatImageButton5 = onView(
+                allOf(withId(R.id.p2RedBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton5.perform(click());
+
+        ViewInteraction appCompatImageButton6 = onView(
+                allOf(withId(R.id.p1GreenBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                4),
+                        isDisplayed()));
+        appCompatImageButton6.perform(click());
+
+        ViewInteraction appCompatImageButton7 = onView(
+                allOf(withId(R.id.p2LightBlueBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                6),
+                        isDisplayed()));
+        appCompatImageButton7.perform(click());
+
+        ViewInteraction appCompatImageButton8 = onView(
+                allOf(withId(R.id.p1PurpleBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                8),
+                        isDisplayed()));
+        appCompatImageButton8.perform(click());
+
+        ViewInteraction appCompatImageButton9 = onView(
                 allOf(withId(R.id.p2OrangeBtn),
                         childAtPosition(
                                 childAtPosition(
@@ -119,7 +185,151 @@ public class SettingsActivityTest {
                                         1),
                                 10),
                         isDisplayed()));
-        appCompatImageButton5.perform(click());
+        appCompatImageButton9.perform(click());
+
+        ViewInteraction appCompatImageButton10 = onView(
+                allOf(withId(R.id.p1LightBlueBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                6),
+                        isDisplayed()));
+        appCompatImageButton10.perform(click());
+
+        ViewInteraction appCompatImageButton11 = onView(
+                allOf(withId(R.id.p2PurpleBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                8),
+                        isDisplayed()));
+        appCompatImageButton11.perform(click());
+
+        ViewInteraction appCompatImageButton12 = onView(
+                allOf(withId(R.id.p1OrangeBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                10),
+                        isDisplayed()));
+        appCompatImageButton12.perform(click());
+
+        ViewInteraction appCompatImageButton13 = onView(
+                allOf(withId(R.id.p2BrownBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                12),
+                        isDisplayed()));
+        appCompatImageButton13.perform(click());
+
+        ViewInteraction appCompatImageButton14 = onView(
+                allOf(withId(R.id.p2RedBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton14.perform(click());
+
+        ViewInteraction appCompatImageButton15 = onView(
+                allOf(withId(R.id.p1BrownBtn),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                12),
+                        isDisplayed()));
+        appCompatImageButton15.perform(click());
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.p1RedBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton2 = onView(
+                allOf(withId(R.id.p1BlueBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton2.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton3 = onView(
+                allOf(withId(R.id.p1GreenBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton3.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton4 = onView(
+                allOf(withId(R.id.p1LightBlueBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton4.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton5 = onView(
+                allOf(withId(R.id.p1PurpleBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton5.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton6 = onView(
+                allOf(withId(R.id.p1OrangeBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton6.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton7 = onView(
+                allOf(withId(R.id.p1BrownBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton7.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton8 = onView(
+                allOf(withId(R.id.p2RedBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton8.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton9 = onView(
+                allOf(withId(R.id.p2BlueBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton9.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton10 = onView(
+                allOf(withId(R.id.p2GreenBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton10.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton11 = onView(
+                allOf(withId(R.id.p2LightBlueBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton11.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton12 = onView(
+                allOf(withId(R.id.p2PurpleBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton12.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton13 = onView(
+                allOf(withId(R.id.p2OrangeBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton13.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton14 = onView(
+                allOf(withId(R.id.p2BrownBtn),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        imageButton14.check(matches(isDisplayed()));
 
         ViewInteraction materialRadioButton = onView(
                 allOf(withId(R.id.radiobtn4x4), withText("4X4"),
@@ -132,6 +342,36 @@ public class SettingsActivityTest {
         materialRadioButton.perform(click());
 
         ViewInteraction materialRadioButton2 = onView(
+                allOf(withId(R.id.radiobtn5x5), withText("5X5"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialRadioButton2.perform(click());
+
+        ViewInteraction materialRadioButton3 = onView(
+                allOf(withId(R.id.radiobtn6x6), withText("6X6"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialRadioButton3.perform(click());
+
+        ViewInteraction materialRadioButton4 = onView(
+                allOf(withId(R.id.song2), withText("Polka"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialRadioButton4.perform(click());
+
+        ViewInteraction materialRadioButton5 = onView(
                 allOf(withId(R.id.song3), withText("Techno"),
                         childAtPosition(
                                 childAtPosition(
@@ -139,27 +379,59 @@ public class SettingsActivityTest {
                                         0),
                                 4),
                         isDisplayed()));
-        materialRadioButton2.perform(click());
+        materialRadioButton5.perform(click());
 
-        ViewInteraction appCompatImageButton6 = onView(
-                allOf(withId(R.id.settingsBackBtn), withContentDescription("Return to main menu button"),
+        ViewInteraction materialRadioButton6 = onView(
+                allOf(withId(R.id.song1), withText("Piano"),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(android.R.id.content),
+                                        withClassName(is("android.widget.LinearLayout")),
                                         0),
                                 0),
                         isDisplayed()));
-        appCompatImageButton6.perform(click());
+        materialRadioButton6.perform(click());
 
-        ViewInteraction appCompatImageButton7 = onView(
-                allOf(withId(R.id.playBtn),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.linearLayout2),
-                                        0),
-                                0),
+        ViewInteraction radioButton2 = onView(
+                allOf(withId(R.id.radiobtn4x4), withText("4X4"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
                         isDisplayed()));
-        appCompatImageButton7.perform(click());
+        radioButton2.check(matches(isDisplayed()));
+
+        ViewInteraction radioButton3 = onView(
+                allOf(withId(R.id.radiobtn5x5), withText("5X5"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        radioButton3.check(matches(isDisplayed()));
+
+        ViewInteraction radioButton4 = onView(
+                allOf(withId(R.id.radiobtn6x6), withText("6X6"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        radioButton4.check(matches(isDisplayed()));
+
+        ViewInteraction radioButton5 = onView(
+                allOf(withId(R.id.song1), withText("Piano"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        radioButton5.check(matches(isDisplayed()));
+
+        ViewInteraction radioButton6 = onView(
+                allOf(withId(R.id.song2), withText("Polka"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        radioButton6.check(matches(isDisplayed()));
+
+        ViewInteraction radioButton7 = onView(
+                allOf(withId(R.id.song3), withText("Techno"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        radioButton7.check(matches(isDisplayed()));
+
+        ViewInteraction radioButton8 = onView(
+                allOf(withId(R.id.song3), withText("Techno"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        radioButton8.check(matches(isDisplayed()));
     }
 
     private static Matcher<View> childAtPosition(
